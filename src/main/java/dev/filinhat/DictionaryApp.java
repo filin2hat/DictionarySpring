d7f804acb9820484b2931ccfe1ce82650ee49e4b
Dmitriy Biryulin <biryulin.develop@gmail.com>
2024-11-27 16:36:53 +0300

    del file repo & service

--- src/main/java/dev/filinhat/DictionaryApp.java
+++ src/main/java/dev/filinhat/DictionaryApp.java
@@ -3,19 +3,9 @@ package dev.filinhat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Запуск Spring Boot приложения
 @SpringBootApplication
 public class DictionaryApp {
     public static void main(String[] args) {
         SpringApplication.run(DictionaryApp.class, args);
     }
 }
-
-// Запуск консольного приложения
-//public class DictionaryApp {
-//    public static void main(String[] args) {
-//        ApplicationContext context = new AnnotationConfigApplicationContext(DictionaryConfiguration.class);
-//        UserInteraction userInteraction = context.getBean(UserInteraction.class);
-//        userInteraction.start();
-//    }
-//}
